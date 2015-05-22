f811a95ebe1517a1f75c57702b6f67a9f0d69967
pwnd00d <austinpwnd00dzhou@gmail.com>
2015-05-22 09:24:36 -0500

    i rike chicken

---
+++ Dot.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class dot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class Dot
+{
+    // instance variables - replace the example below with your own
+    private int x,y;
+
+    /**
+     * Constructor for objects of class dot
+     */
+    public Dot()
+    {
+     
+        
+    }
+}
