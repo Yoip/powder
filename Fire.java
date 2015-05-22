23109978a30c55e00ba4c5636418174f9aab4b2a
pwnd00d <austinpwnd00dzhou@gmail.com>
2015-05-22 09:35:01 -0500

    i rike korean bbq sao

---
+++ Fire.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Fire here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fire extends Dot
+{
+   
+
+    /**
+     * Constructor for objects of class Fire
+     */
+    public Fire()
+    {
+        
+    }
+
+    
+    public void interact(Dot d)
+    {
+    
+    }
+}
